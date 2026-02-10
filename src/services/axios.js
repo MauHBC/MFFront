929728e797144315e80b1836bba4f622b3fa5ec3
MauHBC <mauriciohbcorrea@gmail.com>
2026-02-10 06:48:04 -0300

    Release: ajustes do site 2

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // testes locais
-  baseURL: "http://localhost:3006",
+  // baseURL: "http://localhost:3006",
 
   // subir para produção
-  // baseURL: "/api"
+  baseURL: ""
 });
